e4b87c95e5da5461c468fa64e294e3a100431307
Wildan Mubarok <willnode@wellosoft.net>
2019-01-09 22:55:46 +0700

    Just use typescript

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ console.log(process.env.NODE_ENV);
 
 self.MonacoEnvironment = {
 	getWorkerUrl: function (moduleId, label) {
-		if (label === 'json') {
-			return './json.worker.bundle.js';
-		}
-		if (label === 'css') {
-			return './css.worker.bundle.js';
-		}
-		if (label === 'html') {
-			return './html.worker.bundle.js';
-		}
-		if (label === 'typescript' || label === 'javascript') {
-			return './ts.worker.bundle.js';
-		}
-		return './editor.worker.bundle.js';
+		return './ts.worker.bundle.js';
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,6 @@ module.exports = {
 	mode: 'development',
 	entry: {
 		"app": './index.js',
-		"editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
-		"json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
-		"css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
-		"html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
 		"ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
 	},
 	output: {
