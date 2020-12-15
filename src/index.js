7fe08b7180d94a39b2e43060073a721326c1f273
Wildan M <willnode@wellosoft.net>
2020-12-15 13:55:11 +0700

    Eval push/pop

--- src/index.js
+++ src/index.js
@@ -1,106 +1,181 @@
-import * as monaco from 'monaco-editor';
-import beautify from 'js-beautify';
+import * as monaco from "monaco-editor";
+import beautify from "js-beautify";
 
 console.log(process.env.NODE_ENV);
 
-self.MonacoEnvironment = {
-	getWorkerUrl: function (moduleId, label) {
-		return './ts.worker.js';
-	}
-}
+/* Editor Initialization */
 
-const editor = monaco.editor.create(document.getElementById('container'), {
-	value: sessionStorage['decoder-text'] || [
-		'function x() {',
-		'\tconsole.log("Hello world!");',
-		'}'
-	].join('\n'),
-	language: 'javascript',
-	wordWrap: "on",
-	automaticLayout: true,
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    return "./ts.worker.js";
+  },
+};
+
+const editor = monaco.editor.create(document.getElementById("container"), {
+  value:
+    sessionStorage["decoder-text"] ||
+    ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+  language: "javascript",
+  wordWrap: "on",
+  automaticLayout: true,
 });
 
 editor.getModel().onDidChangeContent(() => {
-	sessionStorage['decoder-text'] = utils.text;
+  sessionStorage["decoder-text"] = utils.text;
 });
 
 const utils = {
-	get selectedText() {
-		return editor.getModel().getValueInRange(editor.getSelection());
-	},
-	set selectedText(text) {
-		var selection = editor.getSelection();
-		var range = new monaco.Range(selection.startLineNumber, selection.startColumn, selection.endLineNumber, selection.endColumn);
-		var id = { major: 1, minor: 1 };
-		var op = {identifier: id, range: range, text: text, forceMoveMarkers: true};
-		editor.executeEdits("my-source", [op]);
-	},
-	get startSelection() {
-		var selection = editor.getSelection();
-		return editor.getModel().getOffsetAt(selection.getStartPosition())
-	},
-	get endSelection() {
-		var selection = editor.getSelection();
-		return editor.getModel().getOffsetAt(selection.getEndPosition())
-	},
-	get text() {
-		return editor.getValue();
-	},
-	set text(v) {
-		editor.setValue(v);
-	},
-	selectAllIfNone () {
-		if(this.startSelection == this.endSelection) {
-			editor.setSelection(editor.getModel().getFullModelRange())
-		}
-	}
-}
+  get selectedText() {
+    return editor.getModel().getValueInRange(editor.getSelection());
+  },
+  set selectedText(text) {
+    var selection = editor.getSelection();
+    var range = new monaco.Range(
+      selection.startLineNumber,
+      selection.startColumn,
+      selection.endLineNumber,
+      selection.endColumn
+    );
+    var id = { major: 1, minor: 1 };
+    var op = {
+      identifier: id,
+      range: range,
+      text: text,
+      forceMoveMarkers: true,
+    };
+    editor.executeEdits("my-source", [op]);
+  },
+  get startSelection() {
+    var selection = editor.getSelection();
+    return editor.getModel().getOffsetAt(selection.getStartPosition());
+  },
+  get endSelection() {
+    var selection = editor.getSelection();
+    return editor.getModel().getOffsetAt(selection.getEndPosition());
+  },
+  get text() {
+    return editor.getValue();
+  },
+  set text(v) {
+    editor.setValue(v);
+  },
+  selectAllIfNone() {
+    if (this.startSelection == this.endSelection) {
+      editor.setSelection(editor.getModel().getFullModelRange());
+    }
+  },
+};
 window.utils = utils;
 
-window.evalStr = function() {
-	utils.selectedText = JSON.stringify(eval("(" + utils.selectedText + ")"));
-}
+/* Evaluation Tools */
 
-window.evalSelectiveStr = function() {
+window.stackEval = [];
+
+window.evalStr = function () {
+  if (window.stackEval.length > 0) {
+    utils.selectedText = JSON.stringify(
+      eval(`
+			(function(){
+				${stackEval.slice(-1)[0]}
+				return ${utils.selectedText};
+			})();
+		`)
+    );
+  } else {
+    utils.selectedText = JSON.stringify(eval("(" + utils.selectedText + ")"));
+  }
+};
+
+window.evalPush = function () {
+  if (utils.startSelection != utils.endSelection) {
+    window.stackEval.push(
+      (window.stackEval.slice(-1)[0] || "") + utils.selectedText + "\n"
+    );
+    console.log("stackEval pushed, current evaluation:");
+    console.log(window.stackEval.slice(-1)[0]);
+  } else {
+    console.log("Nothing selected");
+  }
+};
+window.evalPop = function () {
+  window.stackEval.pop();
+  console.log("stackEval popped, current evaluation:");
+  console.log(stackEval.slice(-1)[0]);
+};
+
+window.evalAuto = function () {
+  utils.selectAllIfNone();
+  var r = /(?:^|;)\s*(?:var|const|let)\s+(_0x[0-9a-fA-F]+)/gm,
+    w,
+    v = [],
+    text = utils.selectedText;
+  while ((w = r.exec(window.stackEval.slice(-1)[0])) !== null) v.push(w[1]);
+  console.log("Captured variables:");
+  console.log(v);
+  v.forEach((t) => {
+    text = text.replace(
+      new RegExp(t + "(\\[.+?\\]|\\(.+?\\)|\\b)", "g"),
+      function (token) {
+        try {
+          var result = eval(`
+			(function(){
+				${stackEval.slice(-1)[0]}
+				return ${token};
+			})();
+		`);
+          if (typeof result == "string" || typeof result == "number") {
+            return JSON.stringify(result);
+          } else {
+            return token;
+          }
+        } catch (error) {
+          return token;
+        }
+      }
+    );
+  });
+  utils.selectedText = text;
+};
+
+/* Formatting Tools */
+
+window.beautify = function () {
+  utils.text = beautify(utils.text, { indent_size: 2 });
+};
+
+window.simplifyString = function () {
+  utils.selectAllIfNone();
+  var replaced = utils.selectedText
+    .replace(/"(\\"|[^"])*?"/g, function (m) {
+      return JSON.stringify(eval(m));
+    })
+    .replace(/'(\\"|[^'])*?'/g, function (m) {
+      return JSON.stringify(eval(m));
+    });
+  utils.selectedText = replaced;
+};
+
+window.simplifyAccess = function () {
+  utils.selectAllIfNone();
+  utils.selectedText = utils.selectedText.replace(
+    /\["([\w_][\w\d_]*?)"\]/g,
+    ".$1"
+  ).replace(
+    /\['([\w_][\w\d_]*?)'\]/g,
+    ".$1"
+  );
+};
+
+window.simplifyVar = function () {
+  utils.selectAllIfNone();
+  console.log("Coming Soon!");
+};
+
+window.simplifyHex = function () {
 	utils.selectAllIfNone();
-	var replaced = utils.selectedText.replace(/"(\\"|[^"])*?"/g, function(m) {
-		return JSON.stringify(eval(m))
-	}).replace(/'(\\"|[^'])*?'/g, function(m) {
-		return JSON.stringify(eval(m))
-	});
-	utils.selectedText = replaced
-}
+	console.log("Coming Soon!");
+  };
 
-window.expandBracket = function() {
-	editor.getAction('editor.action.selectToBracket').run()
-}
-
-window.arraySubtitution = function () {
-	var txt = utils.selectedText;
-	var valname = /([_\w\d]+)\s*=\s*$/.exec(utils.text.substring(0, utils.startSelection))[1]
-	var obj = eval(txt)
-	if (Array.isArray(obj)) {
-		var replaced = utils.text.replace(new RegExp(valname + "\\[(\\d+)\\]", "g"), function(_, p1) {
-			return JSON.stringify(obj[p1])
-		});
-		utils.text = replaced;
-	} else {
-		var replaced = utils.text.replace(new RegExp(valname, "g"), function(_, p1) {
-			return JSON.stringify(obj)
-		});
-		utils.text = replaced;
-	}
-}
-
-window.beautify = function() {
-	utils.text = beautify(utils.text, {indent_size: 2});
-}
-
-window.simplifyAccess = function() {
-	utils.selectAllIfNone();
-	utils.selectedText = utils.selectedText.replace(/\["([\w_][\w\d_]*?)"\]/g, ".$1");
-}
-
-window.gotoRepo = function() {
-	window.open('https://github.com/willnode/deobfuscator', '_blank');
-}
+window.gotoRepo = function () {
+  window.open("https://github.com/willnode/deobfuscator", "_blank");
+};
